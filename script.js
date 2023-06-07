8677a14c5fe19a98d22916a538e73c205ca06998
Danill13and <dandros01@gmail.com>
2023-06-07 18:59:06 +0300

    last

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ document.addEventListener('click', function(e) {
 		}
 	}
 });
+
+const likeButton = document.querySelectorAll('._icon-like'),
+	saveButton = document.querySelectorAll('._icon-save')
+
+function like(e) {
+	if (e.target.style.color != 'rgb(255, 100, 100)') {
+		e.target.style.color = '#FF6464'
+	} else {
+		e.target.style.color = '#000'
+	}
+}
+
+function save(e) {
+	if (e.target.style.color != 'rgb(251, 238, 124)') {
+		e.target.style.color = '#FBEE7C'
+	} else {
+		e.target.style.color = '#000'
+	}
+}
+
+likeButton.forEach(element => {
+	element.addEventListener('click', like)
+});
+
+saveButton.forEach(element => {
+	element.addEventListener('click', save)
+});
